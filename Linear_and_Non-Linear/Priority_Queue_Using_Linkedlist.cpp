ff2d960bdffee196c1a3c7082267d502747dff51
abdullahhussain706 <abdullah706706@gmail.com>
2024-12-15 20:21:27 +0500

    Priority Queue Using Linkedlist

---
+++ Linear_and_Non-Linear/Priority_Queue_Using_Linkedlist.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+	int data;
+	char pri;
+	node *next;
+};
+class Priority_queue
+{
+	node *front,*back,*curr;
+	public:
+		Priority_queue()
+		{
+			front=NULL;
+			back=NULL;
+			curr=NULL;
+		}
+		void enqueue(int data,char priority)
+		{
+			node *newnode=new node;
+			newnode->data=data;
+			newnode->pri=priority;
+			newnode->next=NULL;
+			if(front==NULL)
+			{
+				front =newnode;
+				back=newnode;
+			}
+			else if(priority< front->pri)
+			{
+				newnode->next = front;
+                front = newnode;
+			}
+			else if(priority >= back->pri)
+			{
+				back->next=newnode;
+				back=newnode;
+			}
+			else 
+            {
+            curr = front;
+            while (curr->next != NULL && curr->next->pri < priority)
+            {
+                curr = curr->next;
+    		}
+            	newnode->next = curr->next;
+                curr->next = newnode;
+                }
+		}
+		void display()
+		{
+			curr=front;
+			while(curr!=NULL)
+			{
+				cout<<curr->data<<" "<<"("<<curr->pri<<")"<<"  ";
+				curr=curr->next;
+			}
+		}
+};
+int main()
+{
+	Priority_queue obj;
+	obj.enqueue(15,'1');
+	obj.enqueue(45,'2');
+	obj.enqueue(56,'1');
+	obj.enqueue(89,'5');
+	obj.enqueue(8,'3');
+	obj.display();
+	return 0;
+}
